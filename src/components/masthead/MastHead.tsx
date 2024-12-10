5257f5c30ea61709d63c1b4d865bfea90718836f
Dave Visser <dl.visser@protonmail.com>
2024-12-10 20:15:21 +0100

    Add the logo and burgermenu icon

--- src/components/masthead/MastHead.tsx
+++ src/components/masthead/MastHead.tsx
@@ -2,9 +2,11 @@ import "./MastHead.css";
 import SearchBar from "./searchbar/SearchBar.tsx";
 import VoiceSearch from "./voicesearch/VoiceSearch.tsx";
 import ProfileBlock from "./profileblock/ProfileBlock.tsx";
+import UtilsHeader from "./utilsheader/UtilsHeader.tsx";
 
 export default function MastHead() {
   return (<div className="masthead-container">
+          <UtilsHeader/>
           <div className="search-container">
               <SearchBar/>
               <VoiceSearch/>
