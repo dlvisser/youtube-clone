15ff5ec4f716e7eeb0cd4a2c0f948f0ae164ea8a
Dave Visser <dl.visser@protonmail.com>
2024-12-10 20:05:58 +0100

    Add profile block without functionalities

--- src/components/masthead/MastHead.tsx
+++ src/components/masthead/MastHead.tsx
@@ -1,11 +1,15 @@
 import "./MastHead.css";
 import SearchBar from "./searchbar/SearchBar.tsx";
 import VoiceSearch from "./voicesearch/VoiceSearch.tsx";
+import ProfileBlock from "./profileblock/ProfileBlock.tsx";
 
 export default function MastHead() {
   return (<div className="masthead-container">
-        <SearchBar/>
-        <VoiceSearch/>
+          <div className="search-container">
+              <SearchBar/>
+              <VoiceSearch/>
+          </div>
+          <ProfileBlock/>
   </div>
     )
 }
