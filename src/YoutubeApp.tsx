d7ffcea931ecd93258e8089f252f4ce48473fd1f
Dave Visser <dl.visser@protonmail.com>
2024-12-10 21:15:06 +0100

    Add appdrawer and integrate grid system

--- src/YoutubeApp.tsx
+++ src/YoutubeApp.tsx
@@ -3,19 +3,22 @@ import "./YoutubeApp.css";
 import VideoCardList from "./components/VideoCardList.tsx";
 import ShortsCardList from "./components/ShortsCardList.tsx";
 import GenreList from "./components/GenreList.tsx";
+import Drawer from "./components/appdrawer/Drawer.tsx";
 
 export default function YoutubeApp() {
   return (
-      <div id="yt-app">
+      <div className="yt-app">
           <MastHead/>
-          <GenreList/>
-          <VideoCardList/>
-          <h2>Shorts</h2>
-          <ShortsCardList/>
-          <VideoCardList/>
-          <h2>Shorts</h2>
-          <ShortsCardList/>
-
+          <Drawer/>
+          <div className="content-container">
+              <GenreList/>
+              <VideoCardList/>
+              <h2>Shorts</h2>
+              <ShortsCardList/>
+              <VideoCardList/>
+              <h2>Shorts</h2>
+              <ShortsCardList/>
+          </div>
       </div>
   );
 }
