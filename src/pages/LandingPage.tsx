5ed5f2a6d7d204ccb814abbc24b6cab9cefb987d
Dave Visser <dl.visser@protonmail.com>
2024-12-17 21:15:41 +0100

    Adjust pages to work with grid layout system.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,15 +2,19 @@ import GenreList from "../components/GenreList.tsx";
 import VideoCardList from "../components/VideoCardList.tsx";
 import ShortsCardList from "../components/ShortsCardList.tsx";
 import "./LandingPage.css"
+import Drawer from "../components/appdrawer/Drawer.tsx";
 
 export default function LandingPage() {
     return (
-        <div className="content-container">
-            <GenreList/>
-            <VideoCardList/>
-            <ShortsCardList/>
-            <VideoCardList/>
-            <ShortsCardList/>
-        </div>
+        <>
+            <Drawer/>
+            <div className="content-container">
+                <GenreList/>
+                <VideoCardList/>
+                <ShortsCardList/>
+                <VideoCardList/>
+                <ShortsCardList/>
+            </div>
+        </>
     );
 }
